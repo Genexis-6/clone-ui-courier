a79f15552df26b3e85fda20a97b28efbca4db523
Genexis-6 <victoryazudoni@gmail.com>
2025-05-27 16:20:38 +0100

    finished added the messaging mechanism

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import NavBar from "./components/NavBar";
 import ContentBody from "./Page/ContentBody";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import PayUpage from "./Page/PayUpage";
+import Notification from "./components/Notification";
 
 function App() {
   const [disableNav, setDisableNav] = useState(
     sessionStorage.getItem("disableNav") === "true"
   );
+  const [showNotification, setShowNotification] = useState(false)
+  const [notificationMessage, setNotificationMessage] = useState({
+    msg_:"",
+    type:""
+  })
 
+  const displayNotification = () =>{
+    setShowNotification(true)
+    setTimeout(() => {
+      setShowNotification(false)
+    }, 3000);
+  }
+
+
+  useEffect(() => {
+  displayNotification(); 
+}, []);
+
+  
   return (
     <>
+    {showNotification && <Notification data={notificationMessage}/>}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route element={<ContentBody />} index />
+            <Route element={<ContentBody displayNotification={displayNotification} setNotificationMessage={setNotificationMessage} />} index />
           </Route>
           <Route element={<PayUpage />} path="/pay" />
         </Routes>
---
+++ src/components/Notification.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react"
+import "../assets/css/notification.css"
+export default function Notification({data}){
+    
+    return <div className={`notification`}>
+        <div className={`alert alert-${data.type} w-75 text-center`}>
+            {data.msg_}
+        </div>
+    </div>
+}
